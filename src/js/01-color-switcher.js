0556fa320c16dd3afbf4fe032f48165b7996c5ce
msnow05 <140186070+MSnow05@users.noreply.github.com>
2024-02-15 13:49:39 +0100

    e

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,6 @@
 const startButton = document.querySelector('[data-start]');
 const stopButton = document.querySelector('[data-stop]');
+stopButton.disabled = true
 let intervalId;
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 1677215).toString(16)}`;
@@ -10,15 +11,18 @@ function changeBackgroundColor() {
 }
 
 function startColorSwitcher() {
-  intervalId = setInterval(changeBackgroundColor, 100);
+  intervalId = setInterval(changeBackgroundColor, 1000);
   startButton.disabled = true;
+  stopButton.disabled = false
 }
 
 function stopColorSwitcher() {
   clearInterval(intervalId);
   startButton.disabled = false;
+  stopButton.disabled = true
 }
 
 startButton.addEventListener('click', startColorSwitcher);
+
 stopButton.addEventListener('click', stopColorSwitcher);
- 
+
